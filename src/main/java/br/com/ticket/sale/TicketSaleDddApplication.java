a6d08b102dc454ac2dda27e585201dceccdbd714
Jusley Souza <jusleysouza31@gmail.com>
2026-03-02 16:23:29 -0300

    feat(main-class) Adding the Main class

---
+++ src/main/java/br/com/ticket/sale/TicketSaleDddApplication.java
@@ -0,0 +1,13 @@
+package br.com.ticket.sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSaleDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSaleDddApplication.class, args);
+	}
+
+}
