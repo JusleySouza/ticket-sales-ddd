afaf463ae2f95ce675d3a00aa3fadae36eea8d21
Jusley Souza <jusleysouza31@gmail.com>
2026-03-03 18:01:11 -0300

    refactor(entity) changing the entity to generic

--- src/main/java/br/com/ticket/sale/core/common/domain/Entity.java
+++ src/main/java/br/com/ticket/sale/core/common/domain/Entity.java
@@ -1,7 +1,35 @@
 package br.com.ticket.sale.core.common.domain;
 
-public abstract class Entity {
+import java.util.Objects;
+
+public abstract class Entity<ID> {
+
+    protected ID id;
+
+    public ID getId() {
+        return id;
+    }
 
     public abstract Object toJSON();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        Entity<?> other = (Entity<?>) obj;
+
+        if (this.id == null || other.id == null) {
+            return false;
+        }
+
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
