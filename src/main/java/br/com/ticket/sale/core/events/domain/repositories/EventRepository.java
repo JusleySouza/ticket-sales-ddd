f7baccca1677cbe794a02ca5d2415b75480fc4d7
Jusley Souza <jusleysouza31@gmail.com>
2026-03-08 22:44:40 -0300

    feat(event-repository) creation the EventRepository interface

---
+++ src/main/java/br/com/ticket/sale/core/events/domain/repositories/EventRepository.java
@@ -0,0 +1,20 @@
+package br.com.ticket.sale.core.events.domain.repositories;
+
+import br.com.ticket.sale.core.common.domain.IRepository;
+import br.com.ticket.sale.core.events.domain.entities.event.Event;
+import br.com.ticket.sale.core.events.domain.entities.event.EventId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EventRepository extends IRepository<Event, EventId> {
+
+    void add(Event entity);
+
+    Optional<Event> findById(EventId id);
+
+    List<Event> findAll();
+
+    void delete(EventId id);
+
+}
