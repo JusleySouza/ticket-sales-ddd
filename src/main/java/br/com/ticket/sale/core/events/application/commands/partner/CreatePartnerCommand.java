afc13248ee3e050190cb6346d38d993f62a8ebf1
Jusley Souza <jusleysouza31@gmail.com>
2026-03-09 22:32:37 -0300

    feat(create-partner-command) Adding the CreatePartnerCommand record

---
+++ src/main/java/br/com/ticket/sale/core/events/application/commands/partner/CreatePartnerCommand.java
@@ -0,0 +1,7 @@
+package br.com.ticket.sale.core.events.application.commands.partner;
+
+import br.com.ticket.sale.core.common.domain.value_objects.Name;
+
+public record CreatePartnerCommand(
+        Name name
+) {}
