50b1e8c4e76ef52f488fd1b021aa26c686fbc730
Jusley Souza <jusleysouza31@gmail.com>
2026-03-11 11:53:11 -0300

    feat(order-repository) Adding the OrderRepository interface

---
+++ src/main/java/br/com/ticket/sale/core/events/domain/repositories/OrderRepository.java
@@ -0,0 +1,19 @@
+package br.com.ticket.sale.core.events.domain.repositories;
+
+import br.com.ticket.sale.core.common.domain.IRepository;
+import br.com.ticket.sale.core.events.domain.entities.order.Order;
+import br.com.ticket.sale.core.events.domain.entities.order.OrderId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends IRepository<Order, OrderId> {
+
+    void add(Order order);
+
+    Optional<Order> findById(OrderId id);
+
+    List<Order> findAll();
+
+    void delete(OrderId id);
+}
