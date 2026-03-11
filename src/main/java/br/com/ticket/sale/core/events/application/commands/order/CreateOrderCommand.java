168b7ae8e63cf304378ef9f209ec0ef2c2e68ce5
Jusley Souza <jusleysouza31@gmail.com>
2026-03-11 11:38:20 -0300

    feat(create-order-command) Adding the CreateOrderCommand record

---
+++ src/main/java/br/com/ticket/sale/core/events/application/commands/order/CreateOrderCommand.java
@@ -0,0 +1,13 @@
+package br.com.ticket.sale.core.events.application.commands.order;
+
+import br.com.ticket.sale.core.events.domain.entities.customer.CustomerId;
+import br.com.ticket.sale.core.events.domain.entities.event.EventId;
+import br.com.ticket.sale.core.events.domain.entities.event.section.EventSectionId;
+import br.com.ticket.sale.core.events.domain.entities.event.spot.EventSpotId;
+
+public record CreateOrderCommand(
+        EventId eventId,
+        EventSectionId eventSectionId,
+        EventSpotId eventSpotId,
+        CustomerId customerId
+) {}
