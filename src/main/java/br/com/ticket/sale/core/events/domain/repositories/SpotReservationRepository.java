bd12ab556884401117a28abf5233f99caf0be93f
Jusley Souza <jusleysouza31@gmail.com>
2026-03-11 11:54:22 -0300

    feat(spot-reservation-repository) Adding the SpotReservationRepository interface

---
+++ src/main/java/br/com/ticket/sale/core/events/domain/repositories/SpotReservationRepository.java
@@ -0,0 +1,19 @@
+package br.com.ticket.sale.core.events.domain.repositories;
+
+import br.com.ticket.sale.core.common.domain.IRepository;
+import br.com.ticket.sale.core.events.domain.entities.event.spot.EventSpotId;
+import br.com.ticket.sale.core.events.domain.entities.reservation.SpotReservation;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SpotReservationRepository extends IRepository<SpotReservation, EventSpotId> {
+
+    void add(SpotReservation reservation);
+
+    Optional<SpotReservation> findById(EventSpotId spotId);
+
+    List<SpotReservation> findAll();
+
+    void delete(EventSpotId spotId);
+}
