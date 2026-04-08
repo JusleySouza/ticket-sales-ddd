73797e389b766ccccb499426264a69cd3d5e9e19
Jusley Souza <jusleysouza31@gmail.com>
2026-04-08 17:36:39 -0300

    feat(order-service-test) Adding the Payment Gateway and testing for order cancellation if payment fails and for already reserved slots.

--- src/test/java/br/com/ticket/sale/events/application/order/OrderServiceTest.java
+++ src/test/java/br/com/ticket/sale/events/application/order/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.ticket.sale.events.application.order;
 
 import br.com.ticket.sale.core.events.application.commands.order.CreateOrderCommand;
+import br.com.ticket.sale.core.events.application.gateways.PaymentGateway;
 import br.com.ticket.sale.core.events.application.services.OrderService;
 import br.com.ticket.sale.core.events.domain.entities.customer.Customer;
 import br.com.ticket.sale.core.events.domain.entities.customer.CustomerId;
@@ -17,6 +18,8 @@ import br.com.ticket.sale.core.events.domain.repositories.SpotReservationReposit
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -32,21 +35,26 @@ class OrderServiceTest {
     private EventRepository eventRepository;
     private SpotReservationRepository spotReservationRepository;
 
+    private PaymentGateway paymentGateway;
+
     private OrderService orderService;
 
     @BeforeEach
     void setup() {
+        MockitoAnnotations.openMocks(this);
 
         orderRepository = mock(OrderRepository.class);
         customerRepository = mock(CustomerRepository.class);
         eventRepository = mock(EventRepository.class);
         spotReservationRepository = mock(SpotReservationRepository.class);
+        paymentGateway = mock(PaymentGateway.class);
 
         orderService = new OrderService(
                 orderRepository,
                 customerRepository,
                 eventRepository,
-                spotReservationRepository
+                spotReservationRepository,
+                paymentGateway
         );
     }
 
@@ -56,27 +64,29 @@ class OrderServiceTest {
         EventId eventId = new EventId();
         EventSectionId sectionId = new EventSectionId();
         EventSpotId spotId = new EventSpotId();
+        String cardToken = "card-token-123";
 
-        CreateOrderCommand command = new CreateOrderCommand( eventId, sectionId, spotId, customerId );
+        CreateOrderCommand command = new CreateOrderCommand(eventId, sectionId, spotId, customerId, cardToken);
 
         Customer customer = mock(Customer.class);
         Event event = mock(Event.class);
         EventSection section = mock(EventSection.class);
 
         when(customerRepository.findById(customerId)).thenReturn(Optional.of(customer));
-
         when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
-
         when(event.allowReserveSpot(sectionId, spotId)).thenReturn(true);
-
         when(event.getSections()).thenReturn(Set.of(section));
 
         when(section.getId()).thenReturn(sectionId);
         when(section.getPrice()).thenReturn(BigDecimal.valueOf(100));
 
+        doNothing().when(paymentGateway).pay(anyString(), any());
+
         Order order = orderService.create(command);
 
         assertNotNull(order);
+
+        verify(paymentGateway, times(1)).pay(eq(cardToken), eq(BigDecimal.valueOf(100)));
 
         verify(spotReservationRepository, times(1)).add(any());
         verify(orderRepository, times(1)).add(any());
@@ -91,7 +101,7 @@ class OrderServiceTest {
         EventSectionId sectionId = new EventSectionId();
         EventSpotId spotId = new EventSpotId();
 
-        CreateOrderCommand command = new CreateOrderCommand( eventId, sectionId, spotId, customerId);
+        CreateOrderCommand command = new CreateOrderCommand(eventId, sectionId, spotId, customerId, "token");
 
         when(customerRepository.findById(customerId)).thenReturn(Optional.empty());
 
@@ -106,19 +116,74 @@ class OrderServiceTest {
         EventSectionId sectionId = new EventSectionId();
         EventSpotId spotId = new EventSpotId();
 
-        CreateOrderCommand command =
-                new CreateOrderCommand( eventId, sectionId, spotId, customerId );
+        CreateOrderCommand command = new CreateOrderCommand(eventId, sectionId, spotId, customerId, "token");
 
         Customer customer = mock(Customer.class);
         Event event = mock(Event.class);
 
         when(customerRepository.findById(customerId)).thenReturn(Optional.of(customer));
-
         when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
-
         when(event.allowReserveSpot(sectionId, spotId)).thenReturn(false);
 
         RuntimeException exception = assertThrows(RuntimeException.class, () -> orderService.create(command));
         assertEquals("Spot not available", exception.getMessage());
+    }
+
+    @Test
+    void mustThrowExceptionWhenSpotAlreadyReserved() {
+        CustomerId customerId = new CustomerId();
+        EventId eventId = new EventId();
+        EventSectionId sectionId = new EventSectionId();
+        EventSpotId spotId = new EventSpotId();
+
+        CreateOrderCommand command = new CreateOrderCommand(eventId, sectionId, spotId, customerId, "token");
+
+        Customer customer = mock(Customer.class);
+        Event event = mock(Event.class);
+        EventSection section = mock(EventSection.class);
+
+        when(customerRepository.findById(customerId)).thenReturn(Optional.of(customer));
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(event.allowReserveSpot(sectionId, spotId)).thenReturn(true);
+        when(event.getSections()).thenReturn(Set.of(section));
+
+        when(section.getId()).thenReturn(sectionId);
+
+        doThrow(new DataIntegrityViolationException("error")).when(spotReservationRepository).add(any());
+
+        RuntimeException exception = assertThrows( RuntimeException.class, () -> orderService.create(command));
+        assertEquals("Spot already reserved", exception.getMessage());
+    }
+
+    @Test
+    void mustCancelOrderWhenPaymentFails() {
+        CustomerId customerId = new CustomerId();
+        EventId eventId = new EventId();
+        EventSectionId sectionId = new EventSectionId();
+        EventSpotId spotId = new EventSpotId();
+
+        String cardToken = "card-token-123";
+
+        CreateOrderCommand command = new CreateOrderCommand(eventId, sectionId, spotId, customerId, cardToken);
+
+        Customer customer = mock(Customer.class);
+        Event event = mock(Event.class);
+        EventSection section = mock(EventSection.class);
+
+        when(customerRepository.findById(customerId)).thenReturn(Optional.of(customer));
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(event.allowReserveSpot(sectionId, spotId)).thenReturn(true);
+        when(event.getSections()).thenReturn(Set.of(section));
+
+        when(section.getId()).thenReturn(sectionId);
+        when(section.getPrice()).thenReturn(BigDecimal.valueOf(100));
+
+        doThrow(new RuntimeException("Payment error")).when(paymentGateway).pay(anyString(), any());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> orderService.create(command));
+
+        assertEquals("Payment failed", exception.getMessage());
+
+        verify(orderRepository, times(1)).add(any());
     }
 }
