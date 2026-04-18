bd8d84988e7f16ad0bc31bf6447227e30c46b331
Jusley Souza <jusleysouza31@gmail.com>
2026-04-18 14:06:22 -0300

    feat(events) adding CustomerNameChangedEvent class

---
+++ src/main/java/br/com/ticket/sale/core/events/domain/events/CustomerNameChangedEvent.java
@@ -0,0 +1,23 @@
+package br.com.ticket.sale.core.events.domain.events;
+
+import br.com.ticket.sale.core.common.domain.value_objects.Name;
+import br.com.ticket.sale.core.common.domain.event.AbstractDomainEvent;
+import br.com.ticket.sale.core.events.domain.entities.customer.CustomerId;
+
+public class CustomerNameChangedEvent extends AbstractDomainEvent {
+
+    private final Name newName;
+
+    public CustomerNameChangedEvent(CustomerId id, Name newName) {
+        super(id, 1);
+        this.newName = newName;
+    }
+
+    public CustomerId getCustomerId() {
+        return (CustomerId) getAggregateId();
+    }
+
+    public Name getNewName() {
+        return newName;
+    }
+}
